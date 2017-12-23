7d1bb3f6628321055bc82e4e34ea94bac620fd36
Banana Warlock <gor.liwentsow@yandex.ru>
2017-12-23 16:11:55 +0300

    Fourth commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Every lost resource must be recovered!");
         System.out.println("Hello World!");
+        System.out.println("Second Commit");
     }
 }
